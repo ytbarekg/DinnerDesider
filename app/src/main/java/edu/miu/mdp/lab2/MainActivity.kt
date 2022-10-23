c34821afd5df11d629466077c69ab7873a09759e
ytbarekg <yhailu@miu.edu>
2022-10-23 16:51:58 -0700

    Final commit

--- app/src/main/java/edu/miu/mdp/lab2/MainActivity.kt
+++ app/src/main/java/edu/miu/mdp/lab2/MainActivity.kt
@@ -2,10 +2,25 @@ package edu.miu.mdp.lab2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var list = ArrayList<String>(Arrays.asList("Hamburger", "Pizza",
+            "Mexican", "American", "Chinese"));
+        decide.setOnClickListener{
+            var selected = list.get(Random().nextInt(list.size))
+            chosenFood.text = selected
+        }
+
+        addFood.setOnClickListener{
+            list.add(newFoodName.text.toString());
+            println(newFoodName.text.toString())
+        }
+
     }
 }
